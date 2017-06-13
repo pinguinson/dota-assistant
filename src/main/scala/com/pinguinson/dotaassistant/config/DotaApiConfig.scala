532f3771f6aeb85916789e1d36806c245590fc03
pinguinson <nikita.penguin@gmail.com>
2017-06-13 22:00:28 +0300

    added application.conf

---
+++ src/main/scala/com/pinguinson/dotaassistant/config/DotaApiConfig.scala
@@ -0,0 +1,23 @@
+package com.pinguinson.dotaassistant.config
+
+import pureconfig.loadConfig
+
+/**
+  * Created by pinguinson on 6/13/2017.
+  */
+object DotaApiConfig {
+
+  case class Endpoints(matchHistory: String, matchDetails: String)
+  case class DotaApiConfig(endpoints: Endpoints, validLobbyTypes: Seq[Int], apiKey: String)
+  private case class DotaApi(dotaApi: DotaApiConfig)
+
+  private val defaultConfig = DotaApiConfig(Endpoints("missing", "missing"), List.empty, "missing")
+
+  lazy val config: DotaApiConfig = loadConfig[DotaApi] match {
+    case Left(ex) =>
+      println(s"No config found: $ex")
+      // Maybe should handle this case differently?
+      defaultConfig
+    case Right(c) => c.dotaApi
+  }
+}
--- src/main/scala/com/pinguinson/dotaassistant/scraper/DotaAPI.scala
+++ src/main/scala/com/pinguinson/dotaassistant/scraper/DotaAPI.scala
@@ -1,5 +1,6 @@
 package com.pinguinson.dotaassistant.scraper
 
+import com.pinguinson.dotaassistant.config.DotaApiConfig.config
 import com.pinguinson.dotaassistant.model.{Results, UserGameInfo, UserHeroPerformance}
 import io.circe._
 import io.circe.generic.auto._
@@ -14,12 +15,6 @@ import scalaj.http._
   */
 class DotaAPI extends StatisticsAsync {
 
-  private val matchHistoryApiEndpoint = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1"
-  private val matchDetailsApiEndpoint = "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1"
-  private val steamApiKey = "226CBA05D5049934B32FBD997D6C537F"
-  private val matchmakingModePublic = 0
-  private val matchmakingModeRanked = 7
-
   case class Match(match_id: Long, lobby_type: Int)
 
   case class Player(account_id: Long, hero_id: Int, kills: Int, deaths: Int, assists: Int) {
@@ -27,15 +22,15 @@ class DotaAPI extends StatisticsAsync {
   }
 
   def fetchUserRecentGames(userId: String): Future[Seq[UserGameInfo]] = {
-    val json = Http(matchHistoryApiEndpoint).params(Seq(
-      "key" -> steamApiKey,
+    val json = Http(config.endpoints.matchHistory).params(Seq(
+      "key" -> config.apiKey,
       "account_id" -> userId
     )).asString.body
 
     val cursor = parse(json).getOrElse(Json.Null).hcursor
     val matches = cursor.downField("result").get[List[Match]]("matches").getOrElse(List.empty)
     val detailsList = matches.filter { m =>
-      m.lobby_type == matchmakingModePublic || m.lobby_type == matchmakingModeRanked
+      config.validLobbyTypes contains m.lobby_type
     } take 10 map { m =>
       getMatchDetails(userId, m.match_id.toString)
     }
@@ -52,8 +47,8 @@ class DotaAPI extends StatisticsAsync {
     */
   def getMatchDetails(userId: String, matchId: String): Future[UserGameInfo] = {
     Future {
-      val json = Http(matchDetailsApiEndpoint).params(Seq(
-        "key" -> steamApiKey,
+      val json = Http(config.endpoints.matchDetails).params(Seq(
+        "key" -> config.apiKey,
         "match_id" -> matchId
       )).asString.body
 
