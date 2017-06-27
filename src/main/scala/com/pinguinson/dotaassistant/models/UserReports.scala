9ac7dd6afecc95adf95d01681fd376cc0145c8a7
pinguinson <nikita.penguin@gmail.com>
2017-06-27 03:42:59 +0300

    updated UserGameInfo to only accept IdentifiedPlayer

--- src/main/scala/com/pinguinson/dotaassistant/models/UserReports.scala
+++ src/main/scala/com/pinguinson/dotaassistant/models/UserReports.scala
@@ -1,7 +1,7 @@
 package com.pinguinson.dotaassistant.models
 
 import com.pinguinson.dotaassistant.models.Outcomes.Outcome
-import com.pinguinson.dotaassistant.models.Players.Player
+import com.pinguinson.dotaassistant.models.Players._
 
 /**
   * Created by pinguinson on 6/26/2017.
@@ -14,5 +14,5 @@ object UserReports {
 
   case class UserRecentGames(player: Player, matches: Seq[UserGameInfo]) extends UserReport
   case class UserHeroPerformance(player: Player, hero: String, matches: Int, winrate: Double) extends UserReport
-  case class UserGameInfo(player: Player, hero: String, outcome: Outcome, kda: String) extends UserReport
+  case class UserGameInfo(player: IdentifiedPlayer, hero: String, outcome: Outcome, kda: String) extends UserReport
 }
