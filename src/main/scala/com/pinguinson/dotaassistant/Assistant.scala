68baef5b3def0191a123d2b6567e496f9755fc87
pinguinson <nikita.penguin@gmail.com>
2017-06-27 03:44:23 +0300

    refactored and documented Assistant code

--- src/main/scala/com/pinguinson/dotaassistant/Assistant.scala
+++ src/main/scala/com/pinguinson/dotaassistant/Assistant.scala
@@ -5,15 +5,14 @@ import java.io.File
 import cats.implicits._
 import com.pinguinson.dotaassistant.models._
 import com.pinguinson.dotaassistant.models.UserReports._
-import com.pinguinson.dotaassistant.models.Players._
 import com.pinguinson.dotaassistant.services._
 import com.pinguinson.dotaassistant.JavaFXExecutionContext.javaFxExecutionContext
-import com.pinguinson.dotaassistant.models.Outcomes.{Loss, Outcome, Victory}
+import com.pinguinson.dotaassistant.models.Outcomes._
 
 import scalafx.Includes._
-import scalafx.application.{JFXApp, Platform}
+import scalafx.application._
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.{Scene, Node}
+import scalafx.scene._
 import scalafx.scene.control._
 import scalafx.scene.image._
 import scalafx.scene.layout._
@@ -87,70 +86,56 @@ object Assistant extends JFXApp {
         suggestions.setText("Failed to parse logs")
       case Some(players) =>
         val (radiantReports, direReports) = new DotaAPI(apiKey).fetchMatchPlayersInfo(players).splitAt(5)
-        // remove previous children
-        radiantBlock.children = List.empty[Node]
-        direBlock.children = List.empty[Node]
+        // set placeholder grids
+        val grids = players.map(buildIconGrid)
+        radiantBlock.children = grids.take(5)
+        direBlock.children = grids.drop(5)
 
-        radiantReports.map(_.bimap(
-          { error =>
-            radiantBlock.children.add(buildIconGrid(Either.left(error)))
-          },
-          { playerMatches =>
-            radiantBlock.children.add(buildIconGrid(Either.right(playerMatches)))
-          }
-        ))
-        direReports.map(_.bimap(
-          { error =>
-            direBlock.children.add(buildIconGrid(Either.left(error)))
-          },
-          { playerMatches =>
-            direBlock.children.add(buildIconGrid(Either.right(playerMatches)))
-          }
-        ))
+        radiantReports.map(_.map { playerMatches =>
+          val index = players.indexOf(playerMatches.head.player.id)
+          radiantBlock.children.set(index, buildIconGrid(playerMatches))
+        })
+
+        direReports.map(_.map { playerMatches =>
+          val index = players.indexOf(playerMatches.head.player.id) - 5
+          direBlock.children.set(index, buildIconGrid(playerMatches))
+        })
     }
   }
 
-  def teamMostPickedToString(team: Seq[HeroPerformance]): Label = {
-    val text = team take 5 map {
-      case HeroPerformance(hero, matches, winrate) =>
-        f"$hero picked $matches times with ${winrate * 100}%.1f%% winrate"
-    } mkString "\n"
-    new Label(text)
+  /**
+    * Build a `VBox` with a hero icon
+    * @param optionalGame optional `UserGameInfo`
+    * @return `VBox` with an `ImageView` containing hero icon or a question mark
+    * if `UserGameInfo` is not present. Background color is set to green if
+    * the game was won, set to red otherwise
+    */
+  def buildIcon(optionalGame: Option[UserGameInfo]): VBox = {
+    val icon = HeroService.getMinimapIcon(optionalGame.map(_.hero))
+    val view = new ImageView(new Image(icon))
+    val bgColor = optionalGame.map(_.outcome) match {
+      case Some(Victory) => Color.DarkSeaGreen
+      case _ => Color.LightCoral
+    }
+    val fill = new BackgroundFill(
+      bgColor,
+      new CornerRadii(0),
+      Insets(0, 0, 0, 0)
+    )
+    val box = new VBox(view)
+    box.setBackground(new Background(Array(fill)))
+    box
   }
 
-  def buildIconGrid(e: Either[ApiError, List[UserGameInfo]]): VBox = {
-
-    def buildImageViewForGame(optionalGame: Option[UserGameInfo]) = {
-      val icon = HeroService.getMinimapIcon(optionalGame.map(_.hero))
-      val view = new ImageView(new Image(icon))
-      val bgColor = optionalGame.map(_.outcome) match {
-        case Some(Victory) => Color.DarkSeaGreen
-        case _ => Color.LightCoral
-      }
-      val fill = new BackgroundFill(
-        bgColor,
-        new CornerRadii(0),
-        Insets(0, 0, 0, 0)
-      )
-      val box = new VBox(view)
-      box.setBackground(new Background(Array(fill)))
-      box
-    }
-
-    val labelText = e.map(_.head.player) match {
-      case Left(error) =>
-        error.toString
-      case Right(IdentifiedPlayer(id)) =>
-        s"Player #$id"
-      case Right(UnknownPlayer) =>
-        s"Unknown player"
-    }
-    val views = e match {
-      case Left(error) =>
-        (1 to 20).map(_ => None).map(buildImageViewForGame)
-      case Right(userGameInfos) =>
-        userGameInfos.map(Some(_)).map(buildImageViewForGame)
-    }
+  /**
+    * Build a labeled icon grid
+    * @param labelText label text
+    * @param optionalGames list of optional `UserGameInfo`'s
+    * @return `VBox` containing `Label` with `labelText` and an
+    * icon grid (one icon per optional `UserGameInfo`)
+    */
+  def buildIconGrid(labelText: String, optionalGames: List[Option[UserGameInfo]]): VBox = {
+    val views = optionalGames.map(buildIcon)
     val label = new Label(labelText)
     val row1 = new HBox {
       children = views.take(10)
@@ -158,7 +143,30 @@ object Assistant extends JFXApp {
     val row2 = new HBox {
       children = views.drop(10)
     }
-
     new VBox(label, row1, row2)
+  }
+
+  /**
+    * Build a placeholder icon grid for `userId`
+    * @param userId user ID
+    * @return `VBox` containing label `userId` and 10x2 grid
+    * with question marks
+    */
+  def buildIconGrid(userId: String): VBox = {
+    val labelText = s"Player #$userId"
+    val optionalGames = List.fill[Option[UserGameInfo]](20)(None)
+    buildIconGrid(labelText, optionalGames)
+  }
+
+  /**
+    * Build an icon grid for the `games`
+    * @param games list of user's `UserGameInfo`s
+    * @return `VBox` containing label (player's id) and 10x2 grid
+    * with icons of the heroes he played
+    */
+  def buildIconGrid(games: List[UserGameInfo]): VBox = {
+    val labelText = s"Player #${games.head.player.id}"
+    val optionalGames = games.map(Some(_))
+    buildIconGrid(labelText, optionalGames)
   }
 }
