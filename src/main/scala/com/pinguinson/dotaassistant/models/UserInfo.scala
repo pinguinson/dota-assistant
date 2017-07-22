98d60e2c778fb3f2eb9ebcb6cf45896e83051eb2
pinguinson <nikita.penguin@gmail.com>
2017-07-22 13:55:40 +0300

    fixed UserInfo's pretty method showing MMR only if both solo and party are known

--- src/main/scala/com/pinguinson/dotaassistant/models/UserInfo.scala
+++ src/main/scala/com/pinguinson/dotaassistant/models/UserInfo.scala
@@ -1,15 +1,13 @@
 package com.pinguinson.dotaassistant.models
 
 case class UserInfo(id: String, nickname: String, solo: Option[Int], party: Option[Int]) {
-  def pretty: String = {
-    val mmr = for {
-      s <- solo
-      p <- party
-    } yield s"Solo: $s | Party: $p"
 
-    mmr match {
-      case Some(str) => s"$nickname | $str"
-      case None => s"$nickname"
+  def pretty: String = {
+
+    def mmrToString(mmr: Option[Int]): String = {
+      mmr.map(_.toString).getOrElse("Unknown")
     }
+
+    s"$nickname | Solo: ${mmrToString(solo)} | Party: ${mmrToString(party)}"
   }
 }
